a9fb545ea7c048af92758a1bcfe80131ca81f369
Martin Sundeqvist <martin.sundequist@gmail.com>
2019-08-20 21:34:42 +0200

    Added function to approximate

--- euler.c
+++ euler.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+double func(double t, double y);
 
 int main(int argc, char **argv) {
-	double y0, step_size;
-	y0 = atof(argv[1]);
-	step_size  = atof(argv[2]);
-	printf("initial value: %f, step-size: %f\n", y0, step_size);
+    double y0, step_size;
+    y0 = atof(argv[1]);
+    step_size  = atof(argv[2]);
+    printf("initial value: %f, step-size: %f\n", y0, step_size);
+}
+
+double func(double t, double y) {
+    return 2 - exp(-4 * t) - 2 * y; 
 }
